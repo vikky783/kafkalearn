9a3586b4f54c4a8624ff44b61ea91d40d605c679
vikky kumar <loveuvikky2@gmail.com>
2020-04-07 14:04:12 +0530

    Test commit

--- src/main/java/com/example/kafkalearn/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafkalearn/kafka/KafkaApplication.java
@@ -2,12 +2,17 @@ package com.example.kafkalearn.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.kafkalearn.kafka.tuturial.KafkaConsumer;
 
 @SpringBootApplication
 public class KafkaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkaApplication.class, args);
+		ApplicationContext context =  SpringApplication.run(KafkaApplication.class, args);
+		KafkaConsumer kc = context.getBean(KafkaConsumer.class);
+		kc.name();
 	}
 
 }
---
+++ src/main/java/com/example/kafkalearn/kafka/tuturial/KafkaConsumer.java
@@ -0,0 +1,12 @@
+package com.example.kafkalearn.kafka.tuturial;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+	
+	public void name() {
+		System.out.println("hello");
+	}
+
+}
